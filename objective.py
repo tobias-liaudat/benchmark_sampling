0883505fc3abb565e082fb977307699b7cfeedec
Jonathan Spence <J.Spence@ed.ac.uk>
2024-06-03 13:19:40 +0100

    changes to objective function

--- objective.py
+++ objective.py
@@ -5,6 +5,16 @@ from benchopt import BaseObjective, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
+    import deepinv as dinv
+    import torch
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 
 
 # The benchmark objective must be named `Objective` and
@@ -12,17 +22,20 @@ with safe_import_context() as import_ctx:
 class Objective(BaseObjective):
 
     # Name to select the objective in the CLI and to display the results.
-    name = "Ordinary Least Squares"
+    name = "Sampling PSNR"
 
     # URL of the main repo for this benchmark.
-    url = "https://github.com/#ORG/#BENCHMARK_NAME"
+    url = "https://github.com/tobias-liaudat/benchmark_sampling"
 
     # List of parameters for the objective. The benchmark will consider
     # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     # This means the OLS objective will have a parameter `self.whiten_y`.
     parameters = {
-        'whiten_y': [False, True],
+        'alpha'     :       [1.],
+        'prior'     :       [dinv.optim.ScorePrior(
+                                denoiser=dinv.models.DnCNN(pretrained="download_lipschitz")
+                            ).to(device)],
     }
 
     # List of packages needed to run the benchmark.
@@ -38,34 +51,29 @@ class Objective(BaseObjective):
     # Bump it up if the benchmark depends on a new feature of benchopt.
     min_benchopt_version = "1.5"
 
-    def set_data(self, X, y):
+    def set_data(self, x_true, y, physics):
         # The keyword arguments of this function are the keys of the dictionary
         # returned by `Dataset.get_data`. This defines the benchmark's
         # API to pass data. This is customizable for each benchmark.
-        self.X, self.y = X, y
+        self.x_true, self.y = x_true, y
+        self.physics = physics
 
-        # `set_data` can be used to preprocess the data. For instance,
-        # if `whiten_y` is True, remove the mean of `y`.
-        if self.whiten_y:
-            y -= y.mean(axis=0)
-
-    def evaluate_result(self, beta):
+    def evaluate_result(self, x_est):
         # The keyword arguments of this function are the keys of the
         # dictionary returned by `Solver.get_result`. This defines the
         # benchmark's API to pass solvers' result. This is customizable for
         # each benchmark.
-        diff = self.y - self.X @ beta
 
         # This method can return many metrics in a dictionary. One of these
         # metrics needs to be `value` for convergence detection purposes.
         return dict(
-            value=.5 * diff @ diff,
+            PSNR = dinv.utils.metric.cal_psnr(x_est, self.x_true),
         )
 
     def get_one_result(self):
         # Return one solution. The return value should be an object compatible
         # with `self.evaluate_result`. This is mainly for testing purposes.
-        return dict(beta=np.zeros(self.X.shape[1]))
+        return dict(x_est = self.x_true)
 
     def get_objective(self):
         # Define the information to pass to each solver to run the benchmark.
@@ -74,6 +82,7 @@ class Objective(BaseObjective):
         # benchmark's API for passing the objective to the solver.
         # It is customizable for each benchmark.
         return dict(
-            X=self.X,
             y=self.y,
+            physics = self.physics,
+            prior = self.prior
         )
