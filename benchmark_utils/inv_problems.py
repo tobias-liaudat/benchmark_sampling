57bf64ce6b548c48d16c0da14b83ab9d9fd9fff6
Tobias Liaudat <tobiasliaudat@gmail.com>
2024-06-04 10:32:26 +0200

    add define prior function

--- benchmark_utils/inv_problems.py
+++ benchmark_utils/inv_problems.py
@@ -18,3 +18,20 @@ def define_physics(inv_problem, noise_model, **kwargs):
 
     return physics
 
+
+def define_prior_model(prior_model, **kwargs):
+
+    if prior_model == "dncnn_lipschitz_gray":
+        prior = dinv.optim.ScorePrior(
+            denoiser=dinv.models.DnCNN(
+                pretrained="download_lipschitz",
+                in_channels=1,
+                out_channels=1,
+                device="cpu"
+            )
+        )
+    else:
+        raise NotImplementedError("Prior model {:s} not yet implemented.".format(prior_model))
+
+
+    return prior 
